b5b57ce0816ae854ac09b0f8262b688b20ce541a
Erwan Peres <eperes@jebidouille.com>
2024-10-30 09:07:43 -0400

    add first and last name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true, length: {maximun: 25}
+  validates :last_name, presence: true, length: {maximun: 25}
+
 end
