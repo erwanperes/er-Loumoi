2fc6726858927affe02e0f493525f8722285fbd1
Erwan Peres <eperes@jebidouille.com>
2024-10-30 09:20:04 -0400

    modification des views edit et sign_up pour les nouveaux champs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :first_name, presence: true, length: {maximun: 25}
-  validates :last_name, presence: true, length: {maximun: 25}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
 end
