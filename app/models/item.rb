a96d5e92370e45ecd86130c53e00a08ba867007a
Erwan Peres <eperes@jebidouille.com>
2024-10-30 13:32:23 -0400

    suppression migration items et reervation et modeles

--- app/models/item.rb
+++
@@ -1,8 +0,0 @@
-class Item < ApplicationRecord
-  belongs_to :user
-  has_many :reservations
-
-  validates :name, presence: true
-  validates :category, presence: true
-  validates :user_id, presence: true
-end
